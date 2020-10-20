8b6397557c7483f2a74b15778c01bd02c0fe4d4c
Aidan Gilmore <aidangilmore1@gmail.com>
2020-10-20 19:36:08 +0200

    Updated heroku

--- util/database.js
+++ util/database.js
@@ -1,9 +1,17 @@
 import postgres from 'postgres';
 import dotenv from 'dotenv';
+import extractHerokuDatabaseEnvVars from './extractHerokuDatabaseEnvVars';
 
+extractHerokuDatabaseEnvVars();
 dotenv.config();
 
-const sql = postgres();
+const sql =
+  process.env.NODE_ENV === 'production'
+    ? // Heroku needs SSL connections but
+      // has an "unauthorized" certificate
+      // https://devcenter.heroku.com/changelog-items/852
+      postgres({ ssl: { rejectUnauthorized: false } })
+    : postgres();
 
 export async function getProducts() {
   const products = await sql`
